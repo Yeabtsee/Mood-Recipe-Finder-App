4c65e6296ce45981cca7645042ed8689d5494e96
Yeabtsega Tesfaye <yeabtesfayetesfaye519@gmail.com>
2024-11-23 20:44:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'rgb(149,23,3)' }}>
+    <Text>Welcome to Mood-Based Recipe Finder!</Text>
+  </View>
+);
+
+export default App;
